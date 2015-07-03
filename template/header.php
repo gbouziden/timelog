f8af1bbfbd1d4e5ed14eff2b1665bc0b3b56236e
gbouziden <george.bouziden@gmail.com>
2015-07-03 12:20:25 -0500

    Create header.php

---
+++ template/header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Logger</title>
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+
+</head>
+<body>
+
+<div class="container-fluid">
+<div class="row header">
+	<div class="col-lg-4 col-lg-offset-2">
+		<img src="image/logo.png" alt="Logo"/>	
+	</div> 
+	
+	<div class="col-lg-4 col-lg-offset-2">
+		
+		 <?php  if(isset($_SESSION['user_name']) && strcmp( $_SESSION['user_name'], "admin" ) != 0 ) {  ?>
+			<ul  class="nav nav-pills">
+              <li><a href='index.php'>Check-In</a></li>
+              <li><a href="worklog.php" target="_blank">View Work Log</a></li>
+              <?php
+			  //echo basename($_SERVER['HTTP_REFERER']);
+              if(basename($_SERVER['HTTP_REFERER'])=='login.php'){
+				  $_SESSION['logout_page'] = 'login.php';
+			  }elseif(basename($_SERVER['HTTP_REFERER'])=='ldap_login.php'){
+				  $_SESSION['logout_page'] = 'ldap_login.php';
+			  }elseif(basename($_SERVER['HTTP_REFERER'])=='ldap_login_banc.php'){
+				  $_SESSION['logout_page'] = 'ldap_login_banc.php';
+			  }elseif(basename($_SERVER['HTTP_REFERER'])=='ldap_login_cnbalva.php'){	
+			  	  $_SESSION['logout_page'] = 'ldap_login_cnbalva.php';  
+			  }
+			  ?>
+              <li><a href="logout.php?redirect=<?php echo $_SESSION['logout_page']; ?>">Sign-out</a></li>  
+            </ul>
+		<?php 	} ?>
+		
+	</div> 
+	
+	
+</div>
+</br>
+</br>
+
+
+
+	<div align = "center">
+		<img src="image/smallpsd.jpg" width = "500" height = "120"/>	
+	</div> 
+ 
